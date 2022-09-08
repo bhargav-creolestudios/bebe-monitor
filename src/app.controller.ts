9472b7d306a31865ba7d631e6ef3f33d35f83a24
CS-dharmesh <dharmesh.nayak@creolestudios.com>
2022-09-08 11:19:57 +0530

    -Token generate API development

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { v4 as uuidv4 } from 'uuid';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/getToken')
+  getToken(
+    @Body('senderUserName') senderUserName: string,
+    @Body('reciverUserName') reciverUserName: string
+  ) {
+    const roomName = uuidv4();
+    const senderToken = this.appService.getToken(senderUserName, roomName);
+    const reciverToken = this.appService.getToken(reciverUserName, roomName);
+    return {
+      senderToken: senderToken,
+      reciverToken: reciverToken
+    };
+  }
+
+  @Get('/rooms')
+  async getRoomList() {
+    try {
+      return {
+        Rooms: await this.appService.getRoomList()
+      };
+    } catch (error) {
+      console.log('Error :', error);
+    }
+  }
+
+  @Post('/createRoom')
+  createRoom(@Body('roomName') roomName: string) {
+    this.appService.createRoom(roomName);
+    return {
+      message: 'Successfully create room'
+    };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,58 @@
 import { Injectable } from '@nestjs/common';
+import { AccessToken, RoomServiceClient } from 'livekit-server-sdk';
 
 @Injectable()
 export class AppService {
+  private apikey = 'APIE4pAgorykoBS';
+  private secretkey = 'oatsORZL78l49EHAcnOyEfMr7Dw9eetX1DZD1YuD1jdB';
+  private livekitHost = 'https://livekit.creole.tech';
+
   getHello(): string {
     return 'Hello World!';
+  }
+  getToken(participantName: string, roomName: string): string {
+    const at = new AccessToken(this.apikey, this.secretkey, {
+      identity: participantName
+    });
+    at.addGrant({
+      roomJoin: true,
+      room: roomName,
+      canPublish: true,
+      canSubscribe: true,
+      roomCreate: true,
+      roomList: true,
+      roomAdmin: true,
+      roomRecord: true,
+      canPublishData: true,
+      recorder: true
+    });
+    return at.toJwt();
+  }
+
+  async getRoomList() {
+    try {
+      const svc = new RoomServiceClient(
+        this.livekitHost,
+        this.apikey,
+        this.secretkey
+      );
+      return await svc.listRooms();
+    } catch (err) {
+      console.log('Error :', err);
+    }
+  }
+  async createRoom(roomName: string) {
+    const opts = {
+      name: roomName,
+      // timeout in seconds
+      emptyTimeout: 10 * 60,
+      maxParticipants: 20
+    };
+    const svc = new RoomServiceClient(
+      this.livekitHost,
+      this.apikey,
+      this.secretkey
+    );
+    await svc.createRoom(opts);
   }
 }
